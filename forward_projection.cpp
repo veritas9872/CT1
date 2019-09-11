7f1ee6f2f5b2b60b06bcb5dc1fdc4bab5bbbe7a9
veritas9872 <veritas9872@gmail.com>
2019-09-11 10:02:02 +0900

    Added const to variables that should not change.

--- forward_projection.cpp
+++ forward_projection.cpp
@@ -6,6 +6,7 @@
  * Instead, data must pass through C arrays.
  * This forces this code to allocate and copy to and from the C arrays to std vectors.
  * This results in two extra memory allocations and one extra zero-initialization.
+ * Moreover, the data access pattern is not aligned and coalesced, making the program memory-bound, not compute bound.
  */
 
 #include "mex.h"
@@ -37,24 +38,24 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 #endif
 
     /* get the value of the scalar input  */
-    size_t num_det_pix = mxGetScalar(prhs[1]);  // Number of detector pixels.
-    float det_pix_len = mxGetScalar(prhs[2]);  // Detector pixel length.
+    const size_t num_det_pix = mxGetScalar(prhs[1]);  // Number of detector pixels.
+    const float det_pix_len = mxGetScalar(prhs[2]);  // Detector pixel length.
 
-    float img_pix_len_x = mxGetScalar(prhs[3]);
-    float img_pix_len_y = mxGetScalar(prhs[4]);
+    const float img_pix_len_x = mxGetScalar(prhs[3]);
+    const float img_pix_len_y = mxGetScalar(prhs[4]);
 
-    float sampling_interval = mxGetScalar(prhs[5]);
-    size_t num_views = mxGetScalar(prhs[6]);
-    float projection_range = mxGetScalar(prhs[7]);  // Should be in degrees, not radians.
+    const float sampling_interval = mxGetScalar(prhs[5]);
+    const size_t num_views = mxGetScalar(prhs[6]);
+    const float projection_range = mxGetScalar(prhs[7]);  // Should be in degrees, not radians.
 
     if ((img_pix_len_x < sampling_interval) || (img_pix_len_y < sampling_interval)) {
         cout << "Warning! Sampling interval is shorter than pixel length!" << endl;
     }
 
     /* get dimensions of the input matrix */
-    size_t num_img_pix_rows = mxGetM(prhs[0]);
-    size_t num_img_pix_cols = mxGetN(prhs[0]);
-    size_t num_elements = mxGetNumberOfElements(prhs[0]);
+    const size_t num_img_pix_rows = mxGetM(prhs[0]);
+    const size_t num_img_pix_cols = mxGetN(prhs[0]);
+    const size_t num_elements = mxGetNumberOfElements(prhs[0]);
 
     cout << "Num cols: " << num_img_pix_cols << ". Num rows: "
          << num_img_pix_rows << ". Total: " << num_elements << '.' << endl;
@@ -62,7 +63,7 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
     // Unavoidable memory copy. No way to transfer data from C array to std vector without memory copy.
     // If memory copy can be removed while vector is still used, please do so. Memory copy is very time consuming.
-    std::vector<float> image(input_array, input_array + num_elements);
+    const std::vector<float> image(input_array, input_array + num_elements);
 
     /* create the output matrix */
     // Sinogram dimensions. M: rows = num_det_pix, N: cols = num_views
@@ -75,9 +76,9 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     output_array = mxGetPr(plhs[0]);
 #endif
 
-    float img_len_x = num_img_pix_cols * img_pix_len_x;
-    float img_len_y = num_img_pix_rows * img_pix_len_y;
-    float det_len = num_det_pix * det_pix_len;
+    const float img_len_x = num_img_pix_cols * img_pix_len_x;
+    const float img_len_y = num_img_pix_rows * img_pix_len_y;
+    const float det_len = num_det_pix * det_pix_len;
 
     if ((det_len * det_len < img_len_x * img_len_y) || (det_len < img_len_x) || (det_len < img_len_y)) {
         cout << "Warning! Detector is too short to cover the entire image!" << endl;
@@ -88,10 +89,10 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
     // Distance from center to source and device have been chosen arbitrarily.
     // They are not very important in parallel-beam geometry.
-    float dist2src = sqrtf(img_len_x * img_len_x + img_len_y * img_len_y + 3);
-    float dist2det = dist2src;
+    const float dist2src = sqrtf(img_len_x * img_len_x + img_len_y * img_len_y + 3);  // Distance to source
+    const float dist2det = dist2src;  // Distance to detector
 
-    size_t num_acquisitions = floorf((dist2src + dist2det) / sampling_interval);
+    const size_t num_acquisitions = floorf((dist2src + dist2det) / sampling_interval);
 
     float phi;
     float det_center_x, det_center_y;
@@ -105,12 +106,13 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     float ray_sum;
     float top_left, top_right, down_left, down_right;
 
-    float EPS = 1E-4;  // Necessary for numerical stability in edge cases of range.
-    float rad = (M_PIf32 / 180);  // (PI / 180) in float.
-    float radian_delta = projection_range / num_views * rad;
-    float img_range_x = static_cast<float>(num_img_pix_cols - 1) * img_pix_len_x / 2;
-    float img_range_y = static_cast<float>(num_img_pix_rows - 1) * img_pix_len_y / 2;
-    float det_offset = static_cast<float>(num_det_pix - 1) * det_pix_len / 2;
+    const float EPS = 1E-4;  // Necessary for numerical stability in edge cases of range.
+    const float rad = (M_PIf32 / 180);  // (PI / 180) in float. Used for degree to radian conversion.
+    const float radian_delta = projection_range / num_views * rad;
+    // An offset is the distance from the center to the center of the last pixel.
+    const float img_offset_x = static_cast<float>(num_img_pix_cols - 1) * img_pix_len_x / 2;
+    const float img_offset_y = static_cast<float>(num_img_pix_rows - 1) * img_pix_len_y / 2;
+    const float det_offset = static_cast<float>(num_det_pix - 1) * det_pix_len / 2;
 
     // Initializing sinogram with 0s. Unavoidable memory allocation by 0. Empty vector is not possible.
     std::vector<float> sinogram(num_det_pix * num_views, 0);
@@ -121,6 +123,7 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     // Perhaps use multi-processing to parallelize the for-loops.
     for (size_t view = 0; view < num_views; view++) {
         // Rotating clockwise with the detector at the top and source at the bottom in the beginning.
+        // Detector center begins at (0, dist2det) in xy coordinates and rotates clockwise.
         // This is different from the radon function in MATLAB. Flip the output left-right to get equivalent outputs.
         phi = view * radian_delta;  // Angle between incoming X-ray and the y-axis.
 
@@ -134,6 +137,7 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
         for (size_t det_pix_idx = 0; det_pix_idx < num_det_pix; det_pix_idx++) {
             // The source and detector have numbering from 0~n-1 from the left when the X-ray direction is up.
+            // xy coordinates are in mm with the object center at the origin.
             // At the start, this means that 0 is on the right of the detector and n-1 is on the left of the detector.
             x_end = det_center_x + (det_offset - det_pix_idx * det_pix_len) * cosf(-phi);
             y_end = det_center_y + (det_offset - det_pix_idx * det_pix_len) * sinf(-phi);
@@ -147,17 +151,19 @@ void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
                 // Ignoring cases where X-rays hit the edges. This will be fixed later.
                 // Alternatively, the input data could be zero-padded to allow this code to work precisely.
                 // EPS necessary for numerical stability.
-                in_range = (-img_range_x <= x_pos) && (x_pos < img_range_x - EPS)
-                        && (-img_range_y + EPS < y_pos) && (y_pos <= img_range_y);
+                in_range = (-img_offset_x <= x_pos) && (x_pos < img_offset_x - EPS)
+                           && (-img_offset_y + EPS < y_pos) && (y_pos <= img_offset_y);
 
                 if (in_range) {
-                    // Changing from xy coordinates to row/column coordinate system of the input image.
-                    col = floorf(img_range_x + x_pos);
-                    row = floorf(img_range_y - y_pos);
-                    dc = img_range_x + x_pos - col;
-                    dr = img_range_y - y_pos - row;
+                    // Changing from xy coordinates to row/column coordinate system of the input image in column-major.
+                    // Each index value is set as the center of each pixel with that index with zero-indexing.
+                    col = floorf(img_offset_x + x_pos);
+                    row = floorf(img_offset_y - y_pos);
+                    dc = img_offset_x + x_pos - col;
+                    dr = img_offset_y - y_pos - row;
 
-                    // Column major indexing of the input.
+                    // Column major indexing of the input image.
+                    // Aligned and coalesced memory access is impossible, making this code memory inefficient.
                     top_left = image.at(col * num_img_pix_rows + row);
                     down_left = image.at(col * num_img_pix_rows + (row + 1));
                     top_right = image.at((col + 1) * num_img_pix_rows + row);
